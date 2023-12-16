cc148388c0ab8462268889c29e02bd2f1e1e6045
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-12-16 17:25:48 -0300

    prossegue com o codigo
    Co-authored-by: JoaoSrbek <JoaoSrbek@users.noreply.github.com>
    Co-authored-by: CauaMoreno <CauaMoreno@users.noreply.github.com>

--- trb.cpp
+++ trb.cpp
@@ -8,4 +8,95 @@ Participantes c/ Matrícula:
 */
 #include <iostream>
 #include <cstring>
+#define M 10
+#define N 100
 using namespace std;
+
+//O trabalho foi feito em conjunto presencialmente, também utilizando o VSCode Live Share
+//Desse modo, mantivemos os mesmos padrões nas nomeclaturas das variáveis
+
+//primeiramente, definimos como as impressoes funcionariam
+//nesse caso, cada um fazerá uma verificação de cada estilo de matriz
+//por padrao, fizemos a impressao em caso da palavra for encontrada dentro da funcao
+//se ela for encontrada, tambem retornara true. Caso contrario, retornará false
+//por fim, no int main, ele imprimirá que não foi encontrada caso as 3 retorne false
+
+bool verifHoriz(char m[M][M], char p[], int l, int c) // feito por Estêvão
+{
+    int tamP = strlen(p),contP=0;
+    for (int i=0;i<l;i++)
+    {
+        for (int j=0;j<c;j++)
+        {
+            if (m[i][j] == p[contP])
+            {
+                contP++;
+                if (contP == tamP)
+                {
+                    cout << "Palavra " << p << " foi localizada horizontalmente a partir de posição (" << i+1 << "," << j+1 << ").";
+                    return true;
+                }
+            }
+            else
+            {
+                contP=0;
+            }
+        }
+    }
+    return false;
+}
+
+bool verifVert(char m[M][M], char p[], int l, int c){ // feito por João 
+    bool encontrei=false;
+    int tamanho = strlen(p);
+    int h=0;
+    for(int i=0;i<l;i++){
+        for(int j=0;j<c;j++){
+            if(m[j][i]==p[h]){
+                h++;
+                if(h == tamanho){
+                    cout<<"Palavra "<<p<<" foi localizada verticalmente a partir de posição ("<<j+1<<","<<i++<<").";
+                    encontrei = true;
+                    return encontrei;
+                }
+            }else{
+                h=0;
+            }
+        }
+    }
+    return encontrei;
+}
+
+bool verifDiag(char m[M][M], char p[], int l, int c){ // feito por Cauã
+    int tamP = strlen(p), contP = 0;
+    for (int i=0;i<l;i++)
+    {
+        for (int j=0;j<c;j++)
+        {
+            while (m[i][j] == p[contP])
+            {
+                
+                contP++;
+                
+            }
+        }
+    }
+}
+
+int main(){
+    char m[M][M],p[N];
+    int n,k,v;
+    cin>>n>>k>>v;
+    cin.ignore();
+    for(int i=0;i<n;i++){
+        cin.getline(p,100);
+        int TAM = strlen(p);
+        while(TAM<4){
+            cin.getline(p,100);
+        }
+        verifHoriz(m, p, k, v);
+        verifVert(m, p, k, v);
+        verifDiag(m, p, k, v);
+    }
+    return 0;
+}
