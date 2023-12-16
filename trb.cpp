5b3168ad13e831f90f6ed6511c1d5a67f64f5ba8
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-12-16 16:37:34 -0300

    cria trb valido com cabecalho

---
+++ trb.cpp
@@ -0,0 +1,11 @@
+/*
+Trabalho DCC5199 - Algoritmos I
+Caça Palavras
+Participantes c/ Matrícula:
+-> Cauã Moreno Lopes Castro - Matrícula: 202335019
+-> Estêvão Barbosa Fiorilo da Rocha - Matrícula: 202335030
+-> João Pedro Ferreira SrBek - Matrícula: 202335034
+*/
+#include <iostream>
+#include <cstring>
+using namespace std;
